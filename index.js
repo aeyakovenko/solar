dfdc468942c4107d159960ac4ab4277bc8eb2be6
anatoly yakovenko <aeyakovenko@gmail.com>
2014-12-27 14:59:59 -0800

    recenter around the sun

--- index.js
+++ index.js
@@ -21,14 +21,17 @@ function getQueryVariable(variable) {
 }
 
 var sun = { mass:1.989e30, name:"sun", radius : 695800000,
+            display:{x:0,y:0},
             position:{x:2.821077216283755E-03*AU, y:-8.812560963927427E-04*AU,z:-1.367319222648302E-04*AU},
             velocity:{x:4.006129074936341E-06*AUD,y:5.206019702028657E-06*AUD,z:-9.767312861198032E-08*AUD}};
 
 var earth = { mass:5.972e24, name:"earth", radius: 6371000,
+              display:{x:0,y:0},
               position : {x:-8.169080016031242e-02*AU, y:9.789642498007118e-01*AU,  z:-1.701348267269417e-04*AU},
               velocity : {x:-1.741913145545285e-02*AUD, y:-1.544709454417245e-03*AUD, z:5.355488200427113e-07*AUD}};
 
 var jupiter = { mass:1.898e27, name:"jupiter", radius: 69911000,
+                display:{x:0,y:0},
                 position:{x:-3.699272789771378E+00*AU, y:3.817832117430814E+00*AU, z:6.684392539978537E-02*AU},
                 velocity:{x:-5.509724185671973E-03*AUD,y:-4.896023689305917E-03*AUD,z:1.436000235561424E-04*AUD}};
 
@@ -92,33 +95,24 @@ function update() {
          var xa = gf * xfact(dist, xx.position, yy.position) / xx.mass;
          var ya = gf * yfact(dist, xx.position, yy.position) / xx.mass;
          var za = gf * zfact(dist, xx.position, yy.position) / xx.mass;
-         var startp = { x: xx.position.x, y: xx.position.y, z:xx.position.z };
-         var startv = { x: xx.velocity.x, y: xx.velocity.y, z:xx.velocity.z };
-         var xchange = Math.pow(secondsPerFrame,2)* xa/2 + xx.velocity.x*secondsPerFrame;
-         xx.position.x = xx.position.x + xchange;
-         var ychange = Math.pow(secondsPerFrame,2)* ya/2 + xx.velocity.y*secondsPerFrame;
-         xx.position.y = xx.position.y + ychange;
-         var zchange = Math.pow(secondsPerFrame,2)* za/2 + xx.velocity.z*secondsPerFrame;
-         xx.position.z = xx.position.z + zchange;
 
-         if (2 == getQueryVariable('velocity')) {
-            xx.velocity.x = startv.x + xa * secondsPerFrame;
-            xx.velocity.y = startv.y + ya * secondsPerFrame;
-            xx.velocity.z = startv.z + za * secondsPerFrame;
-         } else {
-            xx.velocity = { x : (xx.position.x - startp.x)/secondsPerFrame,
-                            y : (xx.position.y - startp.y)/secondsPerFrame, 
-                            z : (xx.position.z - startp.z)/secondsPerFrame};
-         }
+         xx.position.x = xx.position.x + Math.pow(secondsPerFrame,2)* xa/2 + xx.velocity.x*secondsPerFrame;
+         xx.position.y = xx.position.y + Math.pow(secondsPerFrame,2)* ya/2 + xx.velocity.y*secondsPerFrame;
+         xx.position.z = xx.position.z + Math.pow(secondsPerFrame,2)* za/2 + xx.velocity.z*secondsPerFrame;
+
+         xx.velocity.x = xx.velocity.x + xa * secondsPerFrame;
+         xx.velocity.y = xx.velocity.y + ya * secondsPerFrame;
+         xx.velocity.z = xx.velocity.z + za * secondsPerFrame;
       }
    }
 }
 
 function draw() {
-   update();
    var c = document.getElementById('canvas');
    var ctx = c.getContext('2d');
+   ctx.save()
    ctx.clearRect(-size.x,-size.x,2*size.x,2*size.y);
+   ctx.translate(center.x - bodies[0].display.x, center.y - bodies[0].display.y);
    for (ii in bodies) {
       var xx = bodies[ii];
       ctx.fillStyle = 'red';
@@ -126,6 +120,8 @@ function draw() {
       var xp = xx.position.x / height * scale;
       var yp = xx.position.y / height * scale;
       var rr = xx.radius / height * scale;
+      xx.display.x = xp;
+      xx.display.y = yp;
       ctx.beginPath();
       ctx.arc(xp,yp,rr + 0.5,0,2*Math.PI);
       ctx.fill();
@@ -135,10 +131,12 @@ function draw() {
       ctx.fillText(xx.name + " " + distance(xx.position, bodies[0].position) / AU, xp, yp);
       ctx.closePath();
    }
-
+   ctx.restore();
 }
 
-var c = document.getElementById('canvas');
-var ctx = c.getContext('2d');
-ctx.translate(center.x, center.y);
-setInterval(draw, 1000/60);
+function drawloop() {
+   update();
+   draw();
+}
+
+setInterval(drawloop, 1000/60);
